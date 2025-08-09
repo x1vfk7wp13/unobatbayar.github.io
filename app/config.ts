6423b431bc0703b0f39b0dfe78463e23db4ad92a
x1vfk7wp13 <38866249+x1vfk7wp13@users.noreply.github.com>
2025-08-09 22:04:14 +0900

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -11,5 +11,5 @@ export const socialLinks = {
   appstore: "https://apps.apple.com/developer/usukhbayar-batbayar/id1532655863",
   github: "https://github.com/x1vfk7wp13",
   stackoverflow: "https://stackoverflow.com/users/12330629",
-  linkedin: "https://www.linkedin.com/in/rytx88jz24dmyzfg290cad9gnzatcr"
+  linkedin: "https://www.linkedin.com/in/rytx88jz24dmyzfg290"
 };
